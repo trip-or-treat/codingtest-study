a08482f9704e6a17e581cef5bb1d3ceed29e9a3d
honee8583 <honee8583@naver.com>
2024-04-14 17:28:31 +0900

    [백준_G5_1717_유니온파인드] : 집합의 표현

---
+++ week1/dahun/G5_B_1717_집합의표현.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int check = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (check == 0) {
+                union(a, b);
+            } else {
+                if (find(a) == find(b)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    public static void union(int a, int b) {
+        if (parent[a] != parent[b]) {
+            a = find(a);
+            b = find(b);
+        }
+
+        if (a < b) {
+            parent[b] = a;
+        } else if (b < a){
+            parent[a] = b;
+        }
+    }
+
+    public static int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = find(parent[node]);
+    }
+}
