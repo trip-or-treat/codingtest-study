c19ba667925eb317813ae124a7c213605218d65f
rlaclghks123 <rlaclghks123@naver.com>
2024-04-27 14:11:28 +0900

    프로그래머스 Lv1 완주 하지 못한 선수 주석 추가

--- week3/chihwan/완주하지못한선수.js
+++ week3/chihwan/완주하지못한선수.js
@@ -1,3 +1,9 @@
+/*
+  1. participant를 순회하면서 map에 [name, 동명이인 명수] 형태로 담아줍니다.
+  2. complete를 순회하면서 map에서 완주한 선수를 -1해줍니다.
+  3. map을 순회하면서 완주하지 못한 선수를 찾아 출력합니다.
+*/
+
 function solution(participant, completion) {
   const map = new Map();
 
